7f75fae2289af2ce80696bc8f96975006b4efdcc
crossbuilder <crossbuilder@users.noreply.github.com>
2016-06-19 08:47:36 +0200

    * cd to home directory first

--- run_xpra.sh
+++ run_xpra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set +x
 echo xpra user passwd: ${XPRA_PW}
+cd
 #INITENV
 xpra start  --bind-tcp=0.0.0.0:10010 --tcp-auth=password:value=${XPRA_PW} --sharing=yes --start-new-commands=yes --speaker=off  :7007 "$@"
